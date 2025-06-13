61826a52c7d96c420dfc56f9b64c1f078c2450dc
Valant41 <valant045@hotmail.com>
2025-06-13 03:56:25 +0200

    Patch bug no buffer + 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ pour activer les routes
+import { BrowserRouter } from 'react-router-dom';
+import { Buffer } from 'buffer'; // ✅ Ajouté ici pour le support navigateur
+
 import './index.css';
 import App from './App.jsx';
+
+// ✅ Polyfill pour que Buffer soit défini dans le navigateur
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
