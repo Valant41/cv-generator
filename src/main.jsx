32a806a8b0f783fec294cd1b01f414b5a115831e
Valant41 <valant045@hotmail.com>
2025-06-13 03:42:09 +0200

    Patch bug route main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // ✅ ajoute cette ligne
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* ✅ ajoute cette balise autour de <App /> */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
