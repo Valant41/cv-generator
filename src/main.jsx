493319f479291c36fd4dcc88f4b5775f2133ba61
Valant41 <valant045@hotmail.com>
2025-06-13 03:51:54 +0200

    patch bug lettre de motivation

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // ✅ ajoute cette ligne
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ pour activer les routes
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ ajoute cette balise autour de <App /> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
