a6f7315b0310eda7d618c8c16e2a15445e459fd8
Valant41 <valant045@hotmail.com>
2025-06-18 17:57:34 +0200

    Changement logo entreprise header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo + Titre */}
         <Link to="/" className="text-2xl sm:text-3xl font-bold flex items-center gap-2">
-          <img src={logo} alt="Logo" className="w-8 h-8 object-contain" />
+          <img src={logo} alt="LogoEntreprise" className="w-8 h-8 object-contain" />
           <span>
             <span className="font-extrabold">Votre CV</span> <span className="font-light">en ligne</span>
           </span>
